875569d146938cf22f7b8f5082b13764bec65a77
Abdulbasit79 <Abdulbasit700@outlook.com>
2021-12-02 00:18:45 +0000

    done

--- play.js
+++ play.js
@@ -1,22 +1,17 @@
-const net = require('net');
-const connect = function() {
-  const conn = net.createConnection({ 
-    host:"165.227.47.243",
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
     port: 50541,
   });
+
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', data => console.log(data));
-  conn.on('connect', () => {
-    console.log('Succesfully connected to game server');
-    conn.write('Name: As');
-    // conn.write("Move: down")
-  });
+  conn.setEncoding("utf8");
+
   return conn;
-}
+};
 
-console.log("connecting...");
-connect(); 
-
-
-
+console.log("Connecting ...");
+connect();
