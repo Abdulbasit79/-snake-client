6a9ea999d058d966517fb54ef73a99507d8a3c97
Abdulbasit79 <Abdulbasit700@outlook.com>
2021-12-02 04:21:56 +0000

    done

--- client.js
+++ client.js
@@ -1,19 +1,31 @@
-const net = require('net');
+const net = require("net");
+const {IP, PORT} = require("./constant");
+
+
 const connect = function() {
-  const conn = net.createConnection({ 
-    host:"165.227.47.243",
-    port: 50541,
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
   });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', data => console.log(data));
-  conn.on('connect', () => {
-    console.log('Succesfully connected to game server');
-    conn.write('Name: As');
-    conn.write("Move: down")
+
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: AB");
+    conn.write("Move: up");
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 300);
+
+    
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
   });
   return conn;
-}
+};
 
-console.log("connecting...");
-connect(); 
+console.log("Connecting ...");
+// connect();
+module.exports = { connect };
